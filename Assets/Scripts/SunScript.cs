894071f02c17ebfe2773298daec60ccabf882302
Detox24 <ddeturk24@gmail.com>
2013-10-25 21:49:28 -0500

    Added SunScript

---
+++ Assets/Scripts/SunScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunScript : MonoBehaviour 
+{
+	
+	private int SunHealth;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		SunHealth = 100;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+    }
+	
+	// Detect Sun hit and update SunHealth
+	void OnCollisionEnter(Collision collision) 
+	{ 
+  		if(collision.gameObject.tag=="Player")
+		{ 
+			SunHealth -= 10;
+     		Debug.Log("hit"); 
+   		} 
+	
+	}
+}
