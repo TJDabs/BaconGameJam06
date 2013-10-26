b6b5a324ac871a9d1465438eab39473ddef86a89
TJDabs <tjdaberkow@gmail.com>
2013-10-25 21:22:48 -0500

    First Scripts

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	
+	private int health;
+	private bool Clicked = false;
+	
+	// Use this for initialization
+	void Start () {
+		health = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Do purple Power
+		if(Clicked == true && GameManger.Power == "Purple")
+		{
+			Debug.Log("True");
+		}
+	}
+	
+	void OnMouseDown()
+	{
+		Clicked = true;
+
+		if(GameManger.Power == "Yellow")
+		{
+			health -= 5;
+			Debug.Log(health);
+		}
+	}
+	
+	void OnMouseUp()
+	{
+		Clicked = false;
+	}
+
+	
+	void die()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManger : MonoBehaviour {
+	
+	public static string Power;
+	
+	// Use this for initialization
+	void Start () {
+		
+		Power = "Purple";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
